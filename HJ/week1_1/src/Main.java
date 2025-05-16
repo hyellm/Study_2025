359a8aa701a1b8c92e021378dea5037b75ca3ed4
HYOJEONG RYU <flowtothemoon355@gmail.com>
2025-05-17 02:38:51 +0900

    week1_1

--- HJ/week1_1/src/Main.java
+++ HJ/week1_1/src/Main.java
@@ -1,2 +1,33 @@
-public class test {
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+
+        List<Person> personList = new ArrayList<>();
+
+        for (int i=0; i<n; i++){
+            int age = sc.nextInt();
+            String name = sc.next();
+            personList.add(new Person(age, name));
+        }
+
+        Collections.sort(personList, Comparator.comparingInt(p->p.age));
+
+        for (Person p: personList){
+            System.out.println(p.age + " " + p.name);
+        }
+    }
+
+    static class Person{
+        int age;
+        String name;
+
+        Person(int age, String name) {
+            this.age = age;
+            this.name = name;
+        }
+    }
 }
+
